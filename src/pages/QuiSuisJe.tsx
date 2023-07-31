d6b328a6c10cfae0960bcdf6af9d0c11fbc27e8e
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-07-31 18:06:12 +0200

    feat(qui-suis-je): update content

--- src/pages/QuiSuisJe.tsx
+++ src/pages/QuiSuisJe.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Description } from '../components/Description';
 import { websiteConfig } from '../website.config';
 import { PageTitle } from './page-layout/PageTitle';
 import { PublicPageLayoutWithFixedBackgroundImage } from './page-layout/PublicPageLayoutWithFixedBackgroundImage';
@@ -22,45 +23,29 @@ export const QuiSuisJe: FC = () => {
           <SemiTransparentTile className="card text-start text-light fs-5 font-kite-one px-4 py-4">
             <p className="">Bonjour !</p>
             <p>Je suis Isabelle Hervouet.</p>
-            <p className="">
+            <Description>
               Aujourd'hui je me définirais comme une facilitatrice, une accompagnatrice de l'Être,
-              une révélatrice de potentiels.
-            </p>
-            <p>
-              J'ai mené un métier ancré dans la réalité matérielle pendant 25 ans au service des
-              entreprises.
-            </p>
-            <p>
-              En parallèle j'ai toujours été intéressée et attirée par tout ce qui touchait à un
-              monde plus subtil, énergétique, invisible et petit à petit je me suis tournée vers le
-              développement personnel, les thérapies corporelles et les méthodes alternatives
-              faisant intervenir le bien-être du corps et de l'esprit.
-            </p>
-            <p>
-              Je me forme d'abord à la sophrologie auprès de l'Académie de sophrologie de Paris avec
-              le Dr Chéné. Puis je découvre un outil tarologique le référentiel de naissance qui est
-              une méthode créée par Georges Colleuil.
-            </p>
-            <p>
-              Je me forme pendant 4 ans à cet outil thérapeutique et initiatique et je chemine
-              depuis avec le tarot. Depuis je continue à me former, cheminer, transmuter et
-              approfondir mes connaissances et ma pratique en explorant toujours et encore les
-              dimensions du corps, du cœur et de l'esprit dans la matière et le subtil.
-            </p>
-            <p>
-              Aujourd'hui je suis plus à l'écoute de mon corps et de mes intuitions, plus alignée
-              dans mes choix, auteure de ma vie, autonome et libre de vivre mes aspirations pour
-              être à ma juste place à savoir l'accompagnement, l'écoute pour vous transmettre à mon
-              tour les clés pour aborder ce chemin vers Soi le mieux possible.
-            </p>
-            <p>
-              Nous avons tous un chemin à parcourir pour nous reconnecter à notre être profond et
-              aller vers notre mission d'âme.
-            </p>
-            <p>
-              « Tout le chemin de la vie c'est de passer de la peur à l'amour ». Frédéric Lenoir
-            </p>
+              une révélatrice de potentiels. J'ai mené un métier ancré dans la réalité matérielle
+              pendant 25 ans au service des entreprises. En parallèle j'ai toujours été intéressée
+              et attirée par tout ce qui touchait à un monde plus subtil, énergétique, invisible et
+              petit à petit je me suis tournée vers le développement personnel, les thérapies
+              corporelles et les méthodes alternatives faisant intervenir le bien-être du corps et
+              de l'esprit. Je me forme d'abord à la sophrologie auprès de l'Académie de sophrologie
+              de Paris avec le Dr Chéné. Puis je découvre un outil tarologique le référentiel de
+              naissance qui est une méthode créée par Georges Colleuil. Je me forme pendant 4 ans à
+              cet outil thérapeutique et initiatique et je chemine depuis avec le tarot. Depuis je
+              continue à me former, cheminer, transmuter et approfondir mes connaissances et ma
+              pratique en explorant toujours et encore les dimensions du corps, du cœur et de
+              l'esprit dans la matière et le subtil. Aujourd'hui je suis plus à l'écoute de mon
+              corps et de mes intuitions, plus alignée dans mes choix, auteure de ma vie, autonome
+              et libre de vivre mes aspirations pour être à ma juste place à savoir
+              l'accompagnement, l'écoute pour vous transmettre à mon tour les clés pour aborder ce
+              chemin vers Soi le mieux possible. Nous avons tous un chemin à parcourir pour nous
+              reconnecter à notre être profond et aller vers notre mission d'âme. « Tout le chemin
+              de la vie c'est de passer de la peur à l'amour ». Frédéric Lenoir
+            </Description>
 
+            <p className=""></p>
             <p className="mb-0 pb-0"></p>
           </SemiTransparentTile>
         </div>
