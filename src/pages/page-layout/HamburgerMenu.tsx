8cf2c821a533d48b238c2b69b35e58789781218a
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2024-03-11 22:06:38 +0100

    feat(burger-menu): enhnace design

--- src/pages/page-layout/HamburgerMenu.tsx
+++ src/pages/page-layout/HamburgerMenu.tsx
@@ -7,9 +7,12 @@ export const HambugerMenu: FC = () => {
   const isOnHomePage = useMemo(() => {
     return location.pathname === '/';
   }, [location.pathname]);
-  const backgroundImageUrl = '/images/landing-page-background.jpg';
-  const computedBackgroundPositionX = 1;
+  const backgroundImageUrl = '/images/backgrounds/burger-menu.png';
+  const computedBackgroundPositionX = -100;
   const computedBackgroundPositionY = 0;
+  const defaultBackgroundOverlay =
+    'linear-gradient(to bottom,rgba(0, 0, 0, 0.7), rgba(79, 79, 93, 0.20))';
+
   return (
     <>
       <nav className="navbar navbar-dark sticky-top position-absolute top-0 end-0 w-100">
@@ -40,7 +43,7 @@ export const HambugerMenu: FC = () => {
         </div>
       </nav>
       <div
-        className={`offcanvas position-absolute text-bg-bordeaux ${
+        className={`offcanvas position-absolute text-bg-badge-burger-menu ${
           websiteConfig.hamburgerMenuPosition === 'left' ? 'offcanvas-start' : 'offcanvas-end'
         }`}
         tabIndex={-1}
@@ -57,83 +60,89 @@ export const HambugerMenu: FC = () => {
           backgroundRepeat: 'no-repeat',
         }}
       >
-        <div className="container h-100 overflow-y-scroll">
-          <div className="offcanvas-header">
-            <h4 className="offcanvas-title text-light fw-bolder" id="offcanvasDarkNavbarLabel">
-              {websiteConfig.websiteShortTitle}
-            </h4>
-            <button
-              id="hamburger-close-button"
-              type="button"
-              className="btn border border-0"
-              data-bs-dismiss="offcanvas"
-              aria-label="Fermer le menu"
-              title="Fermer le menu"
-            >
-              <span className="btn-close-thick-3 text-white"></span>
-            </button>
-          </div>
-          <div className="offcanvas-body fs-6 pt-0">
-            <ul className="navbar-nav justify-content-start align-items-start flex-grow-1 pe-3">
-              <li className="nav-item w-100 text-start">
-                <div className="d-flex p-1 flex-row flex-wrap align-items-center">
-                  <Link
-                    to="/"
-                    className="text-decoration-none text-light"
-                    aria-current="page"
-                    title="Accueil"
-                    aria-label="Accueil"
-                  >
-                    <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
-                      Accueil
-                    </span>
-                  </Link>
-                  <Link
-                    to="/qui-suis-je"
-                    className="text-decoration-none text-light"
-                    title="Qui suis-je ?"
-                    aria-label="Qui suis-je ?"
-                  >
-                    <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
-                      Qui suis-je ?
-                    </span>
-                  </Link>
-                  {/* <Link to="/temoignages" className="text-decoration-none text-light">
-                    <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
+        <div
+          style={{
+            background: `${defaultBackgroundOverlay}`,
+            height: '100vh',
+          }}
+        >
+          <div className="container h-100 overflow-y-scroll">
+            <div className="offcanvas-header">
+              <h4 className="offcanvas-title text-light fw-bolder" id="offcanvasDarkNavbarLabel">
+                {websiteConfig.websiteShortTitle}
+              </h4>
+              <button
+                id="hamburger-close-button"
+                type="button"
+                className="btn border border-0"
+                data-bs-dismiss="offcanvas"
+                aria-label="Fermer le menu"
+                title="Fermer le menu"
+              >
+                <span className="btn-close-thick-3 text-white"></span>
+              </button>
+            </div>
+            <div className="offcanvas-body fs-6 pt-0">
+              <ul className="navbar-nav justify-content-start align-items-start flex-grow-1 pe-3">
+                <li className="nav-item w-100 text-start">
+                  <div className="d-flex p-1 flex-row flex-wrap align-items-center">
+                    <Link
+                      to="/"
+                      className="text-decoration-none text-light"
+                      aria-current="page"
+                      title="Accueil"
+                      aria-label="Accueil"
+                    >
+                      <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
+                        Accueil
+                      </span>
+                    </Link>
+                    <Link
+                      to="/qui-suis-je"
+                      className="text-decoration-none text-light"
+                      title="Qui suis-je ?"
+                      aria-label="Qui suis-je ?"
+                    >
+                      <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
+                        Qui suis-je ?
+                      </span>
+                    </Link>
+                    {/* <Link to="/temoignages" className="text-decoration-none text-light">
+                    <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
                       Témoignages
                     </span>
                   </Link> */}
-                </div>
-              </li>
-              <li className="nav-item w-100 text-start mt-3">
-                <div className=" w-100 border-bottom border-secondary fs-5 text-light fw-bolder">
-                  Mes offres
-                </div>
-                <div className="d-flex p-1 flex-row flex-wrap align-items-center">
-                  <Link to="/theme-de-naissance-par-le-tarot-de-marseille">
-                    <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
-                      Thème de naissance par le tarot de Marseille
-                    </span>
-                  </Link>
-                  <Link to="/la-trame">
-                    <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
-                      La Trame
-                    </span>
-                  </Link>
-                  <Link to="/#">
-                    <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
-                      Sophrologie
-                    </span>
-                  </Link>
-                  <Link to="/#">
-                    <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
-                      Constellations familiales
-                    </span>
-                  </Link>
-                </div>
-              </li>
+                  </div>
+                </li>
+                <li className="nav-item w-100 text-start mt-3">
+                  <div className=" w-100 border-bottom border-secondary fs-5 text-light fw-bolder">
+                    Mes offres
+                  </div>
+                  <div className="d-flex p-1 flex-row flex-wrap align-items-center">
+                    <Link to="/theme-de-naissance-par-le-tarot-de-marseille">
+                      <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
+                        Thème de naissance par le tarot de Marseille
+                      </span>
+                    </Link>
+                    <Link to="/la-trame">
+                      <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
+                        La Trame
+                      </span>
+                    </Link>
+                    <Link to="/#">
+                      <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
+                        Sophrologie
+                      </span>
+                    </Link>
+                    <Link to="/#">
+                      <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
+                        Constellations familiales
+                      </span>
+                    </Link>
+                  </div>
+                </li>
 
-              {/* <li className="nav-item w-100 text-start mt-3">
+                {/* <li className="nav-item w-100 text-start mt-3">
                 <div className="w-100 border-bottom border-secondary fs-6 text-secondary d-flex p-1 flex-row flex-wrap align-items-center">
                   <span>Mon espace client</span>
                 </div>
@@ -144,29 +153,30 @@ export const HambugerMenu: FC = () => {
                   <LogoutAccountBadge />
                 </div>
               </li> */}
-              <li className="nav-item w-100 text-start mt-3">
-                <div className="w-100 border-bottom border-secondary fs-6 text-light fw-bolder d-flex p-1 flex-row flex-wrap align-items-center">
-                  <span>Crédits</span>
-                </div>
-                <div className="d-flex p-1 flex-row flex-wrap align-items-center">
-                  <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
-                    web design: Henri d'Orgeval
-                  </span>
-                </div>
-              </li>
-              <li className="nav-item w-100 text-start mt-3">
-                <div className="w-100 border-bottom border-secondary fs-6 text-light fw-bolder d-flex p-1 flex-row flex-wrap align-items-center">
-                  <span>Confiance numérique</span>
-                </div>
-                <div className="d-flex p-1 flex-row flex-wrap align-items-center">
-                  <Link to="/mentions-legales">
-                    <span className="badge rounded-pill text-bg-bordeaux fs-7 border border-secondary m-1">
-                      Mentions légales
+                <li className="nav-item w-100 text-start mt-3">
+                  <div className="w-100 border-bottom border-secondary fs-6 text-light fw-bolder d-flex p-1 flex-row flex-wrap align-items-center">
+                    <span>Crédits</span>
+                  </div>
+                  <div className="d-flex p-1 flex-row flex-wrap align-items-center">
+                    <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
+                      web design: Henri d'Orgeval
                     </span>
-                  </Link>
-                </div>
-              </li>
-            </ul>
+                  </div>
+                </li>
+                <li className="nav-item w-100 text-start mt-3">
+                  <div className="w-100 border-bottom border-secondary fs-6 text-light fw-bolder d-flex p-1 flex-row flex-wrap align-items-center">
+                    <span>Confiance numérique</span>
+                  </div>
+                  <div className="d-flex p-1 flex-row flex-wrap align-items-center">
+                    <Link to="/mentions-legales">
+                      <span className="badge rounded-pill text-bg-badge-burger-menu fs-7 border border-secondary m-1">
+                        Mentions légales
+                      </span>
+                    </Link>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
