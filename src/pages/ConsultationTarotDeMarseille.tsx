fc67d16978cf7739ba7088d77644ee7bcb2475d5
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2024-11-10 18:18:44 +0100

    fix(background): less gradient overlay

--- src/pages/ConsultationTarotDeMarseille.tsx
+++ src/pages/ConsultationTarotDeMarseille.tsx
@@ -13,7 +13,7 @@ export const ConsultationTarotDeMarseille: FC = () => {
   return (
     <PublicPageLayoutWithFixedBackgroundImage
       backgroundImageUrl="/images/backgrounds/consultation-tarot-02.webp"
-      backgroundOverlay="linear-gradient(to bottom,rgba(0, 0, 0, 0.4), rgba(79, 79, 93, 0.1))"
+      backgroundOverlay="linear-gradient(to bottom,rgba(0, 0, 0, 0.1), rgba(79, 79, 93, 0.0))"
       backgroundPositionX="60%"
       backgroundPositionY="0%"
       htmlTitle={`Consultation Tarot de Marseille | ${websiteConfig.websiteTitle}`}
